10bdbf6b89493797a9157c50d53015bfca3a7fd3
Volodymyr  Reiman <lidersila7788@gmail.com>
2025-10-16 20:17:14 +0300

    First version of lesson

---
+++ Payable.java
@@ -0,0 +1,5 @@
+public interface Payable {
+  int INCREASE_CAP = 20;
+  boolean increasePay(int percent);
+  
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+/*
+public class Test {
+
+//gpt code test
+    public static void main(String[] args) {
+        Contractor c = new Contractor("Vladimir");
+        c.increasePay(15);
+        c.increasePay(25);
+    }
+} 
+    */
